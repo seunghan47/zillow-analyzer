afd9a513d7af6ae409f2bf5afb57db42792ceedf
Paul Lim <paulslim47@gmail.com>
2026-01-07 22:29:50 -0500

    Initial Spring Boot commit

---
+++ src/main/java/com/house/analyzer/HouseAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.house.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/house/analyzer/HouseAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.house.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
