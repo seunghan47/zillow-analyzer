d3eba939df67f567c95e383793029bdec9d874a2
Paul Lim <paulslim47@gmail.com>
2026-01-13 22:44:22 -0500

    delete

--- src/main/java/com/house/analyzer/controller/Controller.java
+++ src/main/java/com/house/analyzer/controller/Controller.java
@@ -1,6 +1,7 @@
 package com.house.analyzer.controller;
 
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,10 @@ public class Controller {
 
     @PostMapping("/item")
     public void postItem() {
+
+    }
+    @DeleteMapping("/delete")
+    public String deleteItem() {
 
     }
 }
