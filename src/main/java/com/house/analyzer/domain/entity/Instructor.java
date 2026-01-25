d968a4903bef7fcc465c21fd7fe05395f161c51f
Paul Lim <paulslim47@gmail.com>
2026-01-25 12:35:29 -0500

    basic pojo for postgres

--- src/main/java/com/house/analyzer/domain/entity/Instructor.java
+++ src/main/java/com/house/analyzer/domain/entity/Instructor.java
@@ -1,4 +1,29 @@
 package com.house.analyzer.domain.entity;
 
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table (name = "instructor")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Instructor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    private String certification;
 }
--- src/main/java/com/house/analyzer/domain/entity/Member.java
+++ src/main/java/com/house/analyzer/domain/entity/Member.java
@@ -1,9 +1,33 @@
 package com.house.analyzer.domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Entity
 @Table (name = "members")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDate joinedDate;
+
 }
